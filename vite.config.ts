8b338ed7787b34cc1c729b1b5e54ae5f87abe196
Shaw996 <shawliu996@gmail.com>
2024-10-25 15:13:33 +0800

    feat(proj): 🚀 first commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import uni from '@dcloudio/vite-plugin-uni';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
