c5eb99c398f1aa6b325739dac885c24fef41caa4
Shaw996 <shawliu996@gmail.com>
2024-10-30 15:34:40 +0800

    fix(proj): 🐛 修复微信小程序平台无法自动加载文件问题；启动微信小程序平台libVersion丢失问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import path from 'node:path';
+
 import uni from '@dcloudio/vite-plugin-uni';
 import { defineConfig } from 'vite';
 import uniPlatformLoaderPlugin from 'vite-plugin-uni-platform-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni(), uniPlatformLoaderPlugin()],
+  plugins: [
+    uni(),
+    uniPlatformLoaderPlugin({
+      platform: process.env.UNI_PLATFORM,
+      rootDir: path.resolve(__dirname, 'src'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
